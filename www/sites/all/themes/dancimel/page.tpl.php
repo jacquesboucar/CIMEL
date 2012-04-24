fec714979a79dc9fb3d052eea306bdd12431ae03
¨Isseu <countasidy@gmail.com>
2012-04-24 17:44:17 +0000

    user log desactivé

--- www/sites/all/themes/dancimel/page.tpl.php
+++ www/sites/all/themes/dancimel/page.tpl.php
@@ -39,11 +39,11 @@
 		</div>
 	<?php endif; ?>
 
-	<?php if (!$is_admin): ?>
+	<!-- <?php if (!$is_admin): ?>
 		<div id="authorize">
       		      <ul><?php global $user; if ($user->uid != 0) { print '<li class="first">' .t('Logged in as '). '<a href="' .url('user/'.$user->uid). '">' .$user->name. '</a></li>'; print '<li><a href="' .url('user/logout'). '">' .t('Logout'). '</a></li>'; } else { print '<li class="first"><a href="' .url('user'). '">' .t('Login'). '</a></li>'; print '<li><a href="' .url('user/register'). '">' .t('Register'). '</a></li>'; } ?></ul>
 		</div>
-	<?php endif; ?>
+	<?php endif; ?> -->
 
       </div><!-- end header-wrapper -->
 </div> <!-- /header -->
