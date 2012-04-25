b14854c0198d7bfea901494913c1b5a157fba6d0
root <root@sagneboucar-laptop.(none)>
2012-04-25 13:51:47 +0200

    update

--- www/sites/all/themes/dancimel/page.tpl.php
+++ www/sites/all/themes/dancimel/page.tpl.php
@@ -226,5 +226,5 @@
 </div> <!-- end footer wrapper -->
 
 <div style="clear:both"></div>
-<div id="notice"><p>Theme by <a href="http://www.danetsoft.com">Danetsoft</a> and <a href="http://www.danpros.com">Danang Probo Sayekti</a> inspired by <a href="http://www.maksimer.no">Maksimer</a></p></div>
+<div id="notice"><p> <a href="http://www.bantalabs.com">By Bantalabs</a> </p></div>
 </div>
